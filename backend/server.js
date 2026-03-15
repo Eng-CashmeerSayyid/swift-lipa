ebf6f5fed3db72eb306e0c76f89d85a012fc7e67
Yasmin Said <yasminsaid@Yasmins-MacBook-Air.local>
2026-03-15 13:57:38 +0300

    Fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
